e51087fb71578d1f97465f77ed90f4ee4e28097a
bfarinango <briannafarinango3@gmail.com>
2025-06-27 02:17:11 -0700

    trying to fix seeding bugs

--- student-store-api/src/routes/orderRoutes.js
+++ student-store-api/src/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/orderController');
+const controller = require('../controllers/orderControllers');
 
 router.get('/', controller.getAll);
 router.get('/:id', controller.getById);
